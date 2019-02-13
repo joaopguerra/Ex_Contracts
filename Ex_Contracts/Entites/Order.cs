efe0140892f04282c4781da26262d2d7d7afe89c
Joao Guerra <jompamg@gmail.com>
2019-02-12 22:26:50 -0300

    Alteracao da classe Order / criacao dos metodos add e remove

--- Ex_Contracts/Entites/Order.cs
+++ Ex_Contracts/Entites/Order.cs
@@ -11,17 +11,28 @@ namespace Ex_Contracts.Entites
         public DateTime Moment { get; set; }
         public OrderStatus Status { get; set; }
         public Client Client { get; set; }
-        public List<OrderItem> OrderItem { get; set; } = new List<OrderItem>();
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
 
         public Order()
         {
         }
 
-        public Order(DateTime moment, OrderStatus status, Client client)
+        public Order(DateTime moment, OrderStatus status, Client client )
         {
             Moment = moment;
             Status = status;
             Client = client;
         }
+
+        public void addItem (OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void removeItem (OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        
     }
 }
