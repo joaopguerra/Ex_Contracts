c3fd7654caea05f97a3806a57e9736338543df52
Joao Guerra <jompamg@gmail.com>
2019-02-12 22:16:18 -0300

    Criacao das classes Order e OrderItem

--- Ex_Contracts/Entites/Order.cs
+++ Ex_Contracts/Entites/Order.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Ex_Contracts.Entites.Enums;
+using Ex_Contracts.Entites;
 
 namespace Ex_Contracts.Entites
 {
     class Order
     {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> OrderItem { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
     }
 }
