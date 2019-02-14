7e9542894cf6df0daa5956faa431d52c1e9bd188
Joao Guerra <jompamg@gmail.com>
2019-02-13 22:08:16 -0300

    Atualizacao da classe OrderItem

--- Ex_Contracts/Entites/OrderItem.cs
+++ Ex_Contracts/Entites/OrderItem.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Ex_Contracts.Entites.Enums;
 
 namespace Ex_Contracts.Entites
 {
     class OrderItem
     {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Item { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price, Product item)
+        {
+            Quantity = quantity;
+            Price = price;
+            Item = item;
+        }
+
+        public double subTotal()
+        {
+            return Price * Quantity;
+        }
     }
 }
